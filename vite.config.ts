cfbd6df51192826512b6d349d1b2336cfd15dd8f
lisayaa07 <chanipron07112546@gmail.com>
2026-01-22 09:58:54 +0700

    ✨ feat: update API proxy target in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://sx7pwm8x-8080.asse.devtunnels.ms", 
+        target: "https://0bptfqdq-8080.asse.devtunnels.ms", 
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
