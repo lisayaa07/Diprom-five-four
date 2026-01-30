34fc804d93a7f57ed91df3906a6abfcfc406e0d1
lisayaa07 <chanipron07112546@gmail.com>
2026-01-30 09:51:03 +0700

    fix: update API proxy target URL in vite.config.ts for correct endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://1jmg3t41-8080.asse.devtunnels.ms", 
+        target: "https://gcmbmgwt-8080.asse.devtunnels.ms", 
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
