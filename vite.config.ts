52f040bf714b8a9f156d5e2c6851685858c8797a
lisayaa07 <chanipron07112546@gmail.com>
2026-02-02 09:41:45 +0700

    ✨ feat: update Vite configuration to load environment variables for API URL; enhance proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig , loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+   server: {
+      proxy: {
       "/api": {
-        target: "https://gcmbmgwt-8080.asse.devtunnels.ms", 
+        target: env.VITE_API_URL, 
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
@@ -18,4 +22,7 @@ export default defineConfig({
       },
     },
   },
+  
+  }
+   
 });
