5d21b1222015e8f435c665a0afd891343953b3bd
lisayaa07 <chanipron07112546@gmail.com>
2026-02-06 13:12:34 +0700

    delete Router path calc

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 // import Sidebar from "./components/Sidebar";
 import Form from "./Form";
 import Add from "./Add";
-import Calc from "./components/Calc";
+
 import Search_User from "./components/Search_User";
 import Order_Detail from "./components/Order_Detail";
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
           <Route path="/" element={<Navigate to="/form" replace />} />
           <Route path="/form" element={<Form />} />
           <Route path="/add" element={<Add />} />
-          <Route path="/calc" element={<Calc />} />
+         
           <Route path="/search-user" element={<Search_User />} />
           <Route path="/order/:orderId" element={<Order_Detail />} />
         </Routes>
