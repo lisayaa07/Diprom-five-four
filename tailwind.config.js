d9ce9c6ceec4aecce0ea90a80f7666aa0fbf9e4c
lisayaa07 <chanipron07112546@gmail.com>
2026-02-24 16:59:11 +0700

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -6,4 +11,5 @@ export default {
       },
     },
   },
+  plugins: [require("daisyui")], 
 };
